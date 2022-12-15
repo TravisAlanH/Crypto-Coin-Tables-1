9d90f96c833d6f999f5730569f20e389bb2d0693
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2022-12-15 17:02:23 -0600

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import "./App.css";
 import React from "react";
 import Axios from "axios";
+import DataBox from "./Components/DataBox";
+import axios from "axios";
 
 function App() {
-  const [data, setData] = React.useState(true);
+  const [data, setData] = React.useState();
+  // var newData;
+
+  const getData = async () => {
+    const res = await axios({
+      method: "get",
+      url: "https://api.coingecko.com/api/v3/coins/",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    setData(res.data);
+  };
+
+  // async function getData() {
+  //   const response = await Axios.get("https://api.coingecko.com/api/v3/coins/");
+  //   let inputData = await response.data;
+  //   setData(inputData);
+  //   return inputData;
+  // }
+
+  // newData = <DataBox inputData={data} />;
 
   React.useEffect(() => {
-    Axios.get("https://api.minerstat.com/v2/coins").then((response) => {
-      setData(response.data);
-    });
-  }, []);
-
-  // const getData = () => {
-  //   Axios.get("https://api.coingecko.com/api/v3/coins/").then((response) => {
-  //     setData(response);
-  //   });
-  // };
+    // Axios.get("https://api.coingecko.com/api/v3/coins/").then((response) => {
+    //   setData(response.data);
+    // });
+    // getData();
+    getData();
+  }, [data]);
 
   return (
     <div className="App">
-      {/* <button onClick={ }>Data</button> */}
+      {/* <button onClick={get}>Data</button> */}
       {console.log(data)}
-      {/* <p>{data[0].id}</p> */}
+      {/* {newData} */}
     </div>
   );
 }
---
+++ src/Components/DataBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DataBox(inputData) {
+  return (
+    <div>
+      <input type="text">{inputData}</input>
+    </div>
+  );
+}
